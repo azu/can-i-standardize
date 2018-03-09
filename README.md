742d111b6fe1279fa44ddbe12c6ca86963203354
azu <azuciao@gmail.com>
2018-03-09 12:37:58 +0900

    Init

---
+++ README.md
@@ -0,0 +1,44 @@
+# can-i-standardize
+
+Can I Standardize?
+
+## Install
+
+Install with [npm](https://www.npmjs.com/):
+
+    npm install can-i-standardize
+
+## Usage
+
+- [ ] Write usage instructions
+
+## Changelog
+
+See [Releases page](https://github.com/azu/can-i-standardize/releases).
+
+## Running tests
+
+Install devDependencies and Run `npm test`:
+
+    npm i -d && npm test
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/azu/can-i-standardize/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- [github/azu](https://github.com/azu)
+- [twitter/azu_re](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
---
+++ src/is-define.js
@@ -0,0 +1,55 @@
+// MIT © 2018 azu
+"use strict";
+import { html, render } from "lit-html";
+
+/**
+ * Return a promise of `isDefined` boolean.
+ * @param {string} method `Array.prototype.flatten`
+ * @param {string} src prototype library
+ * @returns {Promise<boolean>}
+ */
+export function isDefined(method, src) {
+    const currentLocation = location.href;
+    return new Promise((resolve, reject) => {
+        const iframe = document.createElement("iframe");
+        iframe.src = "about:blank";
+        document.body.appendChild(iframe);
+        const receiveMessage = event => {
+            if (event.origin !== location.origin) {
+                return reject(new Error("Not same origin"));
+            }
+            const isDefined = event.data;
+            resolve(isDefined);
+        };
+        window.addEventListener("message", receiveMessage, false);
+
+        const script = html`<script>
+delete ${method};
+var _importScript = (function (oHead) {
+
+  function loadError (oError) {
+    throw new URIError("The script " + oError.target.src + " is not accessible.");
+  }
+
+  return function (sSrc, fOnload) {
+    var oScript = document.createElement("script");
+    oScript.type = "text\\/javascript";
+    oScript.src = sSrc;
+    oScript.onerror = loadError;
+    if (fOnload) { oScript.onload = fOnload; }
+    oHead.appendChild(oScript);  
+  }
+
+})(document.getElementsByTagName("head")[0]);
+</script>
+<script>
+_importScript("${src}", function(){
+    parent.postMessage(typeof ${method} === "function", "${currentLocation}");
+});
+</script>
+`;
+        render(script, iframe.contentWindow.document.body);
+    }).then(isDefined => {
+        return isDefined;
+    });
+}
